c08ef277f4cc36e0a592e5c50e014e1682a732d1
Morgan <mgregg14@ivytech.edu>
2024-10-05 22:09:56 -0400

    putting auth in, waiting for sign in

---
+++ Middleware/authRoles.js
@@ -0,0 +1,26 @@
+/*will activate when signin page is inserted*/
+
+
+
+ /* const ensureTeacher = (req, res, next) => {
+    if (req.user && req.user.role === 'teacher') {
+        return next();
+    } else {
+        return res.status(403).json({  'Access denied, incorrect credentials' });
+    }
+};
+
+
+
+
+
+const ensureStudent = (req, res, next) => {
+    if (req.user && req.user.role === 'student') {
+        return next();
+    } else {
+        return res.status(403).json({ 'Access denied, incorrect credentials' });
+    }
+};
+
+module.exports = { ensureTeacher, ensureStudent };
+*/
